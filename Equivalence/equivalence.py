4b86931202dbb06eeee7c9f7eb1612d1c5e91256
mignoe <miguel.oliver.2003@hotmail.com>
2022-01-09 11:18:46 -0300

    equivalence.py now creates a truth table of the proposition

--- Equivalence/equivalence.py
+++ Equivalence/equivalence.py
@@ -1,37 +1,64 @@
-import functions
+proposition = input()
+#removing spaces
+proposition = proposition.replace(" ", "")
 
-proposition = "x>y"
-
-functions = {
-    "^": functions.And,
-    "V": functions.Or,
-    "¬": functions.Not,
-    ">": functions.implicate
-        }
+operators = "^V¬>"   
+letters = []
 variables = {}
+
 for i in proposition:
-    if i not in functions:
+    if i not in operators:
         variables[i] = True
+        letters.append(i)
 
-#making everything happen
-resultado = ""
-def result():
-    for i in range(len(proposition)):
-        if proposition[i] in functions:
-            operator = functions[proposition[i]]
-            firstVar = variables[proposition[i - 1]]
-            secondVar = variables[proposition[i + 1 ]]
-            aux = operator(firstVar, secondVar)
-            print(aux)
-        
-def changer():
-    for i in variables:
-        if variables[i] == False:
-            variables[i] = True
-            break
+#puting all values to make the operation
 
+def getResult():
+    result = []
+    for index in range(len(proposition)):
+        if proposition[index] not in operators:
+            result.append(variables[proposition[index]])
+        else:
+            result.append(proposition[index])
 
+    #Applying the operators
+        #Not
+    while "¬" in result:
+        i = result.index("¬")
+        result[i + 1] = not result[i + 1]  
+        result.remove("¬")
+        #And
+    while "^" in result:
+        i = result.index("^")          
+        result[i - 1] = result[i - 1] and result[i + 1]
+        result.remove(result[i + 1])
+        result.remove("^")   
+        #Or
+    while "V" in result:
+        i = result.index("V")          
+        result[i - 1] = result[i - 1] or result[i + 1]
+        result.remove(result[i + 1])
+        result.remove("V")
+        #Implicate
+    while ">" in result:
+        i = result.index(">")          
+        result[i - 1] = not result[i - 1] or result[i + 1]
+        result.remove(result[i + 1])
+        result.remove(">")
 
+    return result
 
+#making the result for all possibilities.
+values = [True, False]
+truthTable = []
+def changeValue(n):
+    for i in values:
+        variables[letters[n]] = i
+        if n == 0:
+            print(variables)
+            truthTable.append(getResult())
+        else:
+            changeValue(n-1)
+changeValue(len(variables) - 1)
 
-
+print(truthTable)
